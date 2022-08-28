e1d2b6bb66c16fe4b870672b5f00bb89c3c1d4d2
cbHasib <hasibul.pbn@gmail.com>
2022-08-28 10:54:48 +0600

    Added Meals

--- js/meal.js
+++ js/meal.js
@@ -1,8 +1,27 @@
-const loadMeals = () => {
-    fetch('www.themealdb.com/api/json/v1/1/search.php?f=a')
-    .then(res => res.json())
-    .then(data => console.log(data))
-}
+const displayMeals = (meals) => {
+  const mealContainer = document.getElementById("meal-container");
 
+  meals.forEach(meal => {
+    // console.log(meal)
+    const {strMealThumb, strMeal, strInstructions} = meal;
+    const div = document.createElement("div");
+  div.classList.add("card");
+  div.innerHTML = `<div>
+    <img src="${strMealThumb}" alt="">
+    </div>
+    <div class="card-body">
+        <h2>${strMeal}</h2>
+        <p>${strInstructions.slice(0, 80)}</p>
+        <button type="button" class="btn">Buy Now</button>
+    </div>`;
+    mealContainer.appendChild(div);
+  })
+};
+
+const loadMeals = () => {
+  fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=c")
+    .then((res) => res.json())
+    .then((data) => displayMeals(data.meals));
+};
 
 loadMeals();
